6315b771613aabbba5d5c85a99fd722879cd50b5
kemetsu kejireu <64202724+kemetsu-kejireu@users.noreply.github.com>
2025-05-19 20:20:58 +0900

    Update environment.ts 20250519

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   supabaseAnonKey: 'XXX',
   signUpRedirectUrl: 'http://localhost/login',
     resetPasswordRedirectUrl: 'http://localhost/reset-password',
-    apiUrl: 'http://localhost:8080/api'
+    apiUrl: '/api'
 };
